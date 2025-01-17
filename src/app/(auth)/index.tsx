a01c9e97b73e80bc0a2b801e1238722922c6acb9
gabriel <gabriel.bil8@gmail.com>
2025-01-17 15:55:20 -0300

    feat: Username on Home screen

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,10 +1,15 @@
+import { H2 } from '@/components';
+import { useAuth } from '@/features/auth/providers';
 import { BrandListSmart } from '@/features/brands/components';
 import React from 'react';
 import { View } from 'react-native';
 
 export default function Home() {
+	const { user } = useAuth();
+
 	return (
 		<View className='flex-1 bg-background'>
+			<H2 className='p-4 text-primary'>Hello {user?.name}!</H2>
 			<BrandListSmart />
 		</View>
 	);
